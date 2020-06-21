c14e4ca9aa884601c2cb6b14a7fd209b37cfaddf
Andryo Marzuki <stabbish@gmail.com>
2020-06-21 15:43:02 +1200

    new post or changes

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,11 @@ git add .
 git commit -m 'deploy changes'
 git push
 cd ..
+cd themes/salt
+git add .
+git commit -m 'deploy changes'
+git push
+cd ../..
 git add .
 git commit -m 'new post or changes'
 git push
