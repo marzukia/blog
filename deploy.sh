ee543e8ae1d2b65cca2215b3afefa29d97af5558
Andryo Marzuki <stabbish@gmail.com>
2022-12-07 08:36:13 +1300

    new post or changes

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
-
-shopt -s extglob
-cd marzukia.github.io/
-git checkout master
-rm -rv .git
-cd ..
 hugo
-cd marzukia.github.io/
-git add .
-git commit -m 'deploy changes'
-git push
-cd ..
-cd themes/salt
-git add .
-git commit -m 'deploy changes'
-git push
-cd ../..
-git add .
-git commit -m 'new post or changes'
-git push
+
+cd marzukia.github.io \
+    && git checkout master \
+    && git pull \
+    && git add . \
+    && git commit -m 'deploy changes' \
+    && git push HEAD:master \
+    && cd ..
+
+git add . \
+    && git commit -m 'new post or changes' \
+    && git HEAD:master
