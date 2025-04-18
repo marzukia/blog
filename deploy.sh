39fe803c074e69b1f1eec03f8b3b54af7adeef58
Andryo Marzuki <stabbish@gmail.com>
2025-04-18 14:53:28 +1000

    fix deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 hugo -d ./marzukia.github.io
 
-cd marzukia.github.io && git add . \
-    && git commit -m 'new post or changes' \
-    && git push HEAD:master
+cd marzukia.github.io
+
+git add .
+git commit -m 'new post or changes'
+git push --force
+
+cd ..
