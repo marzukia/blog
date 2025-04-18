305ee956919b72ffe366e88f5adfe43c1f038aae
Andryo Marzuki <stabbish@gmail.com>
2025-04-18 14:48:33 +1000

    fix metadata

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-hugo
+hugo -d ./marzukia.github.io
 
-cd marzukia.github.io \
-    && git checkout master \
-    && git pull \
-    && git add . \
-    && git commit -m 'deploy changes' \
-    && git push HEAD:master \
-    && cd ..
-
-git add . \
+cd marzukia.github.io && git add . \
     && git commit -m 'new post or changes' \
     && git push HEAD:master
